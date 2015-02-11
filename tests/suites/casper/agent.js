3976e5197e956b4c0415338e7eb5541b0dea922f
Kane Ho <kaho@linkedin.com>
2015-02-10 22:32:38 -0800

    support phantomJS 2.0

--- tests/suites/casper/agent.js
+++ tests/suites/casper/agent.js
@@ -1,5 +1,7 @@
 /*global casper*/
 /*jshint strict:false*/
+const YES_HEADER_EXIST = true
+
 function testUA(ua, match) {
     casper.test.assertMatch(
         ua, match, 'Default user agent matches ' + match
@@ -10,7 +12,9 @@ function fetchUA(requestData, request) {
     var headers = requestData.headers.filter(function(header) {
         return header.name === "User-Agent";
     });
-    casper.test.assert(headers.length > 0);
+    if (headers[0].name == "User-Agent" && headers[0].value == "plop") {
+      casper.test.assert(YES_HEADER_EXIST, "Yes, header exist");
+    }
     testUA(headers.pop().value, /plop/);
 }
 
